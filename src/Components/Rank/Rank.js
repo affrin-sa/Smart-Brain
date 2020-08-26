5a9272714410096b9fc47b38b485f73b15ab6e53
affrin-sa <68148181+affrin-sa@users.noreply.github.com>
2020-08-26 10:59:59 -0700

    Add files via upload

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank =() => {
+    return (
+        <div >
+        <div className='white f3 '>
+      {'Affrin ,your current Rank is ...'}
+        </div>
+        <div className='white f1'>
+        {'#5'}
+        </div>
+        </div>
+    );
+}
+
+export default Rank;
